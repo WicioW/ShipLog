0498c50e5295922518da6c4401aee28660f2225c
codeJockey (Wiktor WK) <wiktorwolakksiazekjob@gmail.com>
2023-05-29 15:53:38 +0200

    Initial commit

---
+++ src/main/java/com/wicio/shiplog/ShipLogApplication.java
@@ -0,0 +1,13 @@
+package com.wicio.shiplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wicio/shiplog/ShipLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wicio.shiplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
