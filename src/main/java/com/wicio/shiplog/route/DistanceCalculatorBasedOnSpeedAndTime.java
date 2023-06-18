4555eb4ef7f81bc649c26597a31f770ed55cb3fc
codeJockey (Wiktor WK) <wiktorwolakksiazekjob@gmail.com>
2023-06-18 19:19:49 +0200

    Clean up

--- src/main/java/com/wicio/shiplog/route/DistanceCalculatorBasedOnSpeedAndTime.java
+++ src/main/java/com/wicio/shiplog/route/DistanceCalculatorBasedOnSpeedAndTime.java
@@ -10,29 +10,29 @@ import java.math.BigDecimal;
 @Slf4j
 public class DistanceCalculatorBasedOnSpeedAndTime {
 
-  public int distanceInMetres(
-          Integer speedOverGroundInKmPerH,
-          long minutes
-  ) {
-    Assert.state(speedOverGroundInKmPerH >= 0, "Speed cannot be negative");
-    Assert.state(minutes >= 0, "Time cannot be negative");
+    public int distanceInMetres(
+            Integer speedOverGroundInKmPerH,
+            long minutes
+    ) {
+        Assert.state(speedOverGroundInKmPerH >= 0, "Speed cannot be negative");
+        Assert.state(minutes >= 0, "Time cannot be negative");
 
-    BigDecimal speedOverGroundInKmPerHBigDecimal = BigDecimal.valueOf(speedOverGroundInKmPerH);
-    BigDecimal speedInMetersPerMin =
-            speedOverGroundInKmPerHBigDecimal
-                    .multiply(
-                            BigDecimal.valueOf(1000d / 60d));
-    BigDecimal distanceInMetres = speedInMetersPerMin.multiply(BigDecimal.valueOf(minutes));
-    return distanceInMetres.intValue();
-  }
+        BigDecimal speedOverGroundInKmPerHBigDecimal = BigDecimal.valueOf(speedOverGroundInKmPerH);
+        BigDecimal speedInMetersPerMin =
+                speedOverGroundInKmPerHBigDecimal
+                        .multiply(
+                                BigDecimal.valueOf(1000d / 60d));
+        BigDecimal distanceInMetres = speedInMetersPerMin.multiply(BigDecimal.valueOf(minutes));
+        return distanceInMetres.intValue();
+    }
 
-  public double distanceInKilometres(
-          Integer speedOverGroundInKmPerH,
-          long hours
-  ) {
-    Assert.state(speedOverGroundInKmPerH >= 0, "Speed cannot be negative");
-    Assert.state(hours >= 0, "Time cannot be negative");
-    return speedOverGroundInKmPerH * hours;
-  }
+    public double distanceInKilometres(
+            Integer speedOverGroundInKmPerH,
+            long hours
+    ) {
+        Assert.state(speedOverGroundInKmPerH >= 0, "Speed cannot be negative");
+        Assert.state(hours >= 0, "Time cannot be negative");
+        return speedOverGroundInKmPerH * hours;
+    }
 
 }
